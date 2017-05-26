bf859f3bea5df0837b804805916689effed5244e
aneesha <aneesha.bakharia@gmail.com>
2017-05-26 13:04:07 +0000

    Save text to Scope.preferences with keys for a specified block ref

--- learningdiary/learningdiary.py
+++ learningdiary/learningdiary.py
@@ -3,8 +3,9 @@
 import pkg_resources
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer
+from xblock.fields import Scope, Integer, String, Dict
 from xblock.fragment import Fragment
+from django.template import Template, Context
 
 
 class LearningDiary(XBlock):
@@ -16,9 +17,23 @@ class LearningDiary(XBlock):
     # self.<fieldname>.
 
     # TO-DO: delete count, and define your own fields.
+
     count = Integer(
         default=0, scope=Scope.user_state,
         help="A simple counter, to show something happening",
+    )
+
+    '''
+    diary = String(
+            default="my journal 2", scope=Scope.preferences,
+            help="A simple store for a cross xblock learning diary",
+    )
+    '''
+    diary_entries = Dict(help="A simple store for a cross xblock learning diary", scope=Scope.preferences)
+
+    block_ref = String(
+            default="diary1", scope=Scope.settings,
+            help="A ref for the block",
     )
 
     def resource_string(self, path):
@@ -33,7 +48,13 @@ class LearningDiary(XBlock):
         when viewing courses.
         """
         html = self.resource_string("static/html/learningdiary.html")
-        frag = Fragment(html.format(self=self))
+
+        t = Template(html)
+        c = Context({'diary_entry':self.diary_entries[self.block_ref], 'count':self.count, 'entries':self.diary_entries})
+        t.render(c)
+
+        #frag = Fragment(html.format(self=self))
+        frag = Fragment(t.render(c))
         frag.add_css(self.resource_string("static/css/learningdiary.css"))
         frag.add_javascript(self.resource_string("static/js/src/learningdiary.js"))
         frag.initialize_js('LearningDiary')
@@ -51,6 +72,21 @@ class LearningDiary(XBlock):
 
         self.count += 1
         return {"count": self.count}
+
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    @XBlock.json_handler
+    def update_diary(self, data, suffix=''):
+        """
+        An example handler, which increments the data.
+        """
+        # Just to show data coming in...
+        #assert data['hello'] == 'world'
+
+        self.diary_entries[self.block_ref] = "Learning Diary updated"
+        return {"diary": self.diary_entries[self.block_ref], "all_entries": self.diary_entries}
+
+
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
--- learningdiary/static/js/src/learningdiary.js
+++ learningdiary/static/js/src/learningdiary.js
@@ -5,7 +5,13 @@ function LearningDiary(runtime, element) {
         $('.count', element).text(result.count);
     }
 
+    function updateDiary(result) {
+        $('#diary_txtinput', element).text(result.diary);
+    }
+
     var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+
+    var handlerUrlDiary = runtime.handlerUrl(element, 'update_diary');
 
     $('p', element).click(function(eventObject) {
         $.ajax({
@@ -15,6 +21,16 @@ function LearningDiary(runtime, element) {
             success: updateCount
         });
     });
+
+    $('#mybtn', element).click(function(eventObject) {
+        $.ajax({
+            type: "POST",
+            url: handlerUrlDiary,
+            data: JSON.stringify({"hello": "world"}),
+            success: updateDiary
+        });
+    });
+
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
