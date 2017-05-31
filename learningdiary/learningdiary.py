c9e4e3cca9074f85e3aae836993016fb2ffa6c73
Maarten <maarten@greenhost.nl>
2017-05-31 12:32:38 +0000

    fix a bug where the learning diary would not work on a new setup

--- learningdiary/learningdiary.py
+++ learningdiary/learningdiary.py
@@ -17,24 +17,25 @@ class LearningDiary(XBlock):
     # self.<fieldname>.
 
     # TO-DO: delete count, and define your own fields.
-
     count = Integer(
-        default=0, scope=Scope.user_state,
+        default=0, scope=Scope.preferences,
         help="A simple counter, to show something happening",
     )
 
-    '''
-    diary = String(
-            default="my journal 2", scope=Scope.preferences,
-            help="A simple store for a cross xblock learning diary",
-    )
-    '''
-    diary_entries = Dict(help="A simple store for a cross xblock learning diary", scope=Scope.preferences)
+    # We'll be abusing Scope.preferences to save the diary_entries because
+    # that's a user-wide field
+    diary_entries = Dict(
+        help="A simple store for a cross xblock learning diary",
+        scope=Scope.preferences)
 
     block_ref = String(
-            default="diary1", scope=Scope.settings,
-            help="A ref for the block",
+        default="diary1", scope=Scope.settings,
+        help="A ref for the block",
     )
+
+    # The text in the diary entry field when the user has not entered anything
+    # yet
+    default_diary_entry = "start typing here"
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -50,18 +51,39 @@ class LearningDiary(XBlock):
         html = self.resource_string("static/html/learningdiary.html")
 
         t = Template(html)
-        c = Context({'diary_entry':self.diary_entries[self.block_ref], 'count':self.count, 'entries':self.diary_entries})
+
+        diary_entry = self.default_diary_entry
+        if self.block_ref in self.diary_entries:
+            diary_entry = self.diary_entries[self.block_ref]
+
+        c = Context({
+            'block_ref': self.block_ref,
+            'diary_entry': diary_entry,
+            'count': self.count,
+            'entries':self.diary_entries
+        })
         t.render(c)
 
         #frag = Fragment(html.format(self=self))
         frag = Fragment(t.render(c))
         frag.add_css(self.resource_string("static/css/learningdiary.css"))
-        frag.add_javascript(self.resource_string("static/js/src/learningdiary.js"))
+        frag.add_javascript(
+            self.resource_string("static/js/src/learningdiary.js"))
         frag.initialize_js('LearningDiary')
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
+    def studio_view(self, context=None):
+        """
+        View for editing which part to show in the XBlock
+        """
+        html = self.resource_string("static/html/learningdiarysettings.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_css(self.resource_string("static/css/learningdiary.css"))
+        frag.initialize_js('LearningDiary')
+        return frag
+
+    # TO-DO: change this handler to perform your own actions.  You may need
+    # more than one handler, or you may not need any handlers at all.
     @XBlock.json_handler
     def increment_count(self, data, suffix=''):
         """
@@ -84,9 +106,10 @@ class LearningDiary(XBlock):
         #assert data['hello'] == 'world'
 
         self.diary_entries[self.block_ref] = "Learning Diary updated"
-        return {"diary": self.diary_entries[self.block_ref], "all_entries": self.diary_entries}
-
-
+        return {
+            "diary": self.diary_entries[self.block_ref],
+            "all_entries": self.diary_entries
+        }
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
